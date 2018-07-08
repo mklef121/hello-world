e507e121978cfdc40f9609f2a789bb53cb3566e2
mklef121 <mklef121@gmail.com>
2018-07-08 21:11:49 +0100

    Late evening commit tests

---
+++ packages.php
@@ -0,0 +1,110 @@
+<?php return array (
+  'ably/ably-php-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Ably\\Laravel\\AblyServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Ably' => 'Ably\\Laravel\\Facades\\Ably',
+    ),
+  ),
+  'barryvdh/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Cors\\ServiceProvider',
+    ),
+  ),
+  'barryvdh/laravel-dompdf' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\DomPDF\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'PDF' => 'Barryvdh\\DomPDF\\Facade',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'jimmyjs/laravel-report-generator' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Jimmyjs\\ReportGenerator\\ServiceProvider',
+    ),
+  ),
+  'laravel/passport' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Passport\\PassportServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'maatwebsite/excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'pusher/pusher-http-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pusher\\Laravel\\PusherServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Pusher' => 'Pusher\\Laravel\\Facades\\Pusher',
+    ),
+  ),
+  'spatie/laravel-fractal' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Fractal\\FractalServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Fractal' => 'Spatie\\Fractal\\FractalFacade',
+    ),
+  ),
+);
